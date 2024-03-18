45ea71282deee159d922818318d3025314142ceb
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-18 21:20:17 +0000

    Add operate function to return a function based on the operator variable

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let num1 = 0;
+let num2 = 0;
+let operator = null;
+
+
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -12,4 +17,16 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+};
+
+function operate(num1, num2, operator) {
+    if(operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2);
+    } else if (operator === "*") {
+        multiply(num1, num2);
+    } else if (operate === "/") {
+        divide(num1, num2);
+    }
 };
