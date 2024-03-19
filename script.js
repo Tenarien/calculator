d09c578d38fd9adb2e68d1a1bcdfc8015a715d78
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-19 20:32:40 +0000

    Completely rewritten logic and massively simplified the process of clicking

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let num1 = 0;
-let num2 = 0;
-let operator = null;
+let num1 = '';
+let num2 = '';
+let operator = '';
+let solution = '';
+
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
+const equal = document.querySelector(".equal");
+const signs = document.querySelectorAll(".sign")
+const clear = document.querySelector(".clear");
 
 
 function add(num1, num2) {
-    return num1 + num2;
+    return num1, num2
 };
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    solution = num1 - num2;
 };
 
 function multiply(num1, num2) {
@@ -20,33 +27,77 @@ function divide(num1, num2) {
 };
 
 function operate(num1, num2, operator) {
-    if(operator === "+") {
-        add(num1, num2);
-    } else if (operator === "-") {
+    if(operator === '+') {
+    } else if (operator === '-') {
         subtract(num1, num2);
-    } else if (operator === "*") {
+    } else if (operator === '*') {
         multiply(num1, num2);
-    } else if (operate === "/") {
+    } else if (operate === '/') {
         divide(num1, num2);
     }
 };
 
-const btn = document.querySelectorAll("button");
 
-btn.forEach(button => {
-    button.addEventListener("click", () => {
-        buttonClick(button.textContent);
+
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => {
+        num1 = num1 + digit.innerHTML;
+        display.innerHTML = num1;
     });
 });
 
-const display = document.querySelector(".display");
+signs.forEach((sign) => {
+    sign.addEventListener("click", () => {
+        operator = sign.innerHTML;
+    });
+});
+
+clear.addEventListener("click", () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    solution = '';
+    display.innerHTML = '';
+});
 
 function buttonClick(button) {
-    let displayNumber = document.createElement("p");
-    displayNumber.textContent = button;
-    display.appendChild(displayNumber);
-    console.log(displayNumber)
+    display.textContent += parseInt(button);
+    if(button === "clear") {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild);
+            num1 = "";
+            num2 = "";
+            operator = "";
+        }
+    }
+    if(button === "+" || button === "-" || button === "/" || button === "*") {
+        operator = button;
+    }
+    if(num1 === "" && button === "+" || button === "-" || button === "/" || button === "*") {
+        while(display.firstChild) {
+            num1 += parseInt(display.textContent);
+            display.removeChild(display.firstChild);
+        }
+    }
+
+    if(num1 > 0 && button === "+" || button === "-" || button === "/" || button === "*" || button === "=") {
+        while(display.firstChild) {
+            num2 += parseInt(display.textContent);
+            display.removeChild(display.firstChild);
+        }
+    }
+    
+    if(button === "=" && num1 !== "" && num2 !== "" && operator !== "") {
+        num1 = parseInt(num1)
+        num2 = parseInt(num2)
+        console.log(num1)
+        console.log(num2)
+        console.log(operator)
+        operate(num1, num2, operator);
+    }
 }
+
+
 
 
 
